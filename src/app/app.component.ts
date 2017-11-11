b5d9311c3f1a07bbbe9f0e6fa841630ed0d3c8bf
justmelnyc <mpachecodesign@gmail.com>
2017-11-11 13:40:37 -0500

    set up core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'root',
   template: `
-    <h1>Prism</h1>
+    <div id="app" class="wrapper">
+      <header>
+        <a href="#">Prism</a>
+        <a href="#">info</a>
+      </header>
+      <section class="intro">
+        <h2>
+          <b>Ahoy there</b> &mdash; <b>I'm a product designer passionate about solving interesting problems from vision to the last 5
+          <small>%</small></b>
+          <br>
+          <span id="magic">(where the magic happens)</span>
+        </h2>
+      </section>
+      <section class="projects">
+        <div class="project">
+          <span class="meta-data code">3D Projection Mapping</span>
+          <a href="#" class="faux-link">Using machine learning to create a better color tool for designers.</a>
+        </div>
+        <div class="project">
+          <span class="meta-data code">Content Creation</span>
+          <a href="#" class="faux-link">The text based RPG game in need of a writer.</a>
+        </div>
+        <div class="project">
+          <span class="meta-data code">Custom LED Installations</span>
+          <a href="#"  class="faux-link">Helping New Yorkers become more flood resilient.</a>
+        </div>
+        <div class="project">
+          <span class="meta-data code">Software Development</span>
+          <a href="#" class="faux-link">Prototyping a behavior change app to help me quit smoking.</a>
+        </div>
+        
+      </section>
+      <footer>
+        <div class="footer-container">
+          <div class="copyright">© <span id="copyright"></span> <a href="index.html">Prism New York</a></div>
+          <div class="courtesy">
+            <div class="email">Email</div>
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
+            <a href="#">LinkedIn</a>
+          </div>
+        </div>
+      </footer>
+    </div>
 
     <router-outlet></router-outlet>
-  `,
-  styles: [``]
+  `
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app.routes';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routes'
+import { AppComponent } from './app.component'
+
+// Core
+import { CoreModule } from './core/core.module'
+
+// Shared/Widget
+import { SharedModule } from './shared/shared.module'
+
+// Feature Modules
+import { ItemModule } from './items/shared/item.module'
+import { UploadModule } from './uploads/shared/upload.module'
+import { UiModule } from './ui/shared/ui.module'
+import { NotesModule } from './notes/notes.module'
+///// End FireStarter
+
+
+
+
+import { environment } from '../environments/environment'
+import { AngularFireModule } from 'angularfire2'
+
+export const firebaseConfig = environment.firebaseConfig
+import { AngularFirestoreModule } from 'angularfire2/firestore'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ItemModule,
+    UiModule,
+    NotesModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
+
